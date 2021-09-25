f6e1b367bfadb54ede9d4cd5362b277044f4de5f
potatosaac <tanisaac3103@yahoo.com.sg>
2021-09-25 11:28:03 +0800

    added stonks file

--- codeitsuisse/routes/stonks.py
+++ codeitsuisse/routes/stonks.py
@@ -7,14 +7,13 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
-def evaluate():
+@app.route('/stonks', methods=['POST'])
+def evaluateprofit():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
+    print(data)
     logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    
 
 
 
